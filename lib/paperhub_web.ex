0fb61974e3e6511460977a58fb426e71d477a8ac
caslu <sepulchrolucas@gmail.com>
2025-02-17 22:37:21 -0300

    add assets, update tailwind and git lfs

--- lib/paperhub_web.ex
+++ lib/paperhub_web.ex
@@ -17,7 +17,7 @@ defmodule PaperhubWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico favicon.svg site.webmanifest favicon-96x96.png robots.txt apple-touch-icon.png web-app-manifest-192x192.png web-app-manifest-512x512.png)
 
   def router do
     quote do
