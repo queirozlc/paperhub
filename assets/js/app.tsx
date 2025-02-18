5076ec0afbfcf7ac57ec40754083592823275241
caslu <sepulchrolucas@gmail.com>
2025-02-18 12:11:58 -0300

    add shadcn, linter and formatting config

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,16 +1,16 @@
-import axios from 'axios';
-import 'phoenix_html';
+import axios from 'axios'
+import 'phoenix_html'
 
-import { createInertiaApp } from '@inertiajs/react';
-import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
 
-axios.defaults.xsrfHeaderName = 'x-csrf-token';
+axios.defaults.xsrfHeaderName = 'x-csrf-token'
 
 createInertiaApp({
-	resolve: async (name) => {
-		return await import(`./pages/${name}.tsx`);
-	},
-	setup({ App, el, props }) {
-		createRoot(el).render(<App {...props} />);
-	},
-});
+  resolve: async (name) => {
+    return await import(`./pages/${name}.tsx`)
+  },
+  setup({ App, el, props }) {
+    createRoot(el).render(<App {...props} />)
+  }
+})
