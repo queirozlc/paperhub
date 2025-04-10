34c0b002dac52a502d2e71a19105af0ebbb69351
caslu <sepulchrolucas@gmail.com>
2025-04-10 19:37:26 -0300

    require unauthenticated user on verify email

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,5 +1,16 @@
 class Users::ConfirmationsController < ApplicationController
+  before_action :require_unauthenticated
+
   def show
     render inertia: "Confirmations/Show", props: { email: params.fetch(:email, "") }
   end
+
+
+
+  private
+    def require_unauthenticated
+      return unless user_signed_in?
+
+      redirect_to authenticated_root_path, flash: { error: "You are already signed in." }
+    end
 end
--- spec/requests/users/sessions_controller_spec.rb
+++ spec/requests/users/sessions_controller_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Users::SessionsController, :inertia do
   describe 'GET /sign_in' do
-    it 'renders sign in inertia component' do
-      get new_user_session_path
+    context 'when user is not logged in' do
+      it 'renders sign in inertia component' do
+        get new_user_session_path
 
-      expect_inertia.to render_component('Auth/SignIn')
+        expect_inertia.to render_component('Auth/SignIn')
+      end
+    end
+
+    context 'when user is logged in' do
+      include_context 'when user is logged in'
+
+      it 'redirects to the root path' do
+        get new_user_session_path
+        expect(response).to redirect_to(authenticated_root_path)
+      end
     end
   end
 end
