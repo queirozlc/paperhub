7d331f7a2a796d4b9da42a567692a76e7790b0e0
caslu <sepulchrolucas@gmail.com>
2025-04-14 01:38:18 -0300

    fix lint rules

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -13,7 +13,6 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/projects", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # Project. As you add validations to Project, be sure to
   # adjust the attributes here as well.
@@ -74,7 +73,7 @@ RSpec.describe "/projects", type: :request do
       it "does not create a new Project" do
         expect {
           post projects_url, params: { project: invalid_attributes }
-        }.to change(Project, :count).by(0)
+        }.not_to change(Project, :count)
       end
 
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
