084e8d0d01beb58525ce9a20540c59487c030c58
caslu <sepulchrolucas@gmail.com>
2025-05-01 13:49:05 -0300

    add project showing with sqid instead of id

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -57,7 +57,7 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      @project = Project.find_by_sqid!(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
@@ -70,7 +70,7 @@ class ProjectsController < ApplicationController
     end
 
     def serialize_project(project)
-      project.as_json
+      project.as_json(methods: :sqid)
     end
 
     def serialize_team(team)
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,9 @@
 class Project < ApplicationRecord
+  include Sqids::Rails::Model
+
   acts_as_tenant :team
   enum :visibility, %i[private public], validate: true, default: :public, prefix: true
   validates :visibility, presence: true
+  has_sqid :long_sqid, min_length: 24
+  has_sqid min_length: 21
 end
