c5541417aaaaf610249ddc15849f913542c9fa42
caslu <sepulchrolucas@gmail.com>
2025-05-01 13:53:26 -0300

    remove unused field

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,5 @@ class Project < ApplicationRecord
   acts_as_tenant :team
   enum :visibility, %i[private public], validate: true, default: :public, prefix: true
   validates :visibility, presence: true
-  has_sqid :long_sqid, min_length: 24
   has_sqid min_length: 21
 end
