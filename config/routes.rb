42ae2c3fd64673af3caa277fa307947f4f12d2eb
Bruno Remeikis <bremeikis@softexpert.com>
2025-05-08 09:36:00 -0300

    Performs search by title in the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   authenticated :user do
     root to: "documents#index", as: :authenticated_root
+    get "documents" => "documents#index"
     get "onboarding" => "users/onboarding#edit", as: :user_onboarding
     patch "onboarding" => "users/onboarding#update", as: :user_onboarding_update
     resources :documents, except: %i[index new edit]
     delete "documents" => "documents#destroy_all", as: :destroy_all_documents
     resources :teams, only: %i[create]
-    namespace :users do
-      resources :teams, only: %i[update], controller: "profile"
-    end
+  end
+
+  namespace :users do
+    resources :teams, only: %i[update], controller: "profile"
   end
 
   root to: redirect("/sign_in")
