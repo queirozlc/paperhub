85637e2491c2218bea3c1624e345974a11f3115e
Bruno Remeikis <bremeikis@softexpert.com>
2025-05-20 00:22:11 -0300

    Move string-utils / Formata arquivos / Remove comentários

--- app/frontend/utils/string-utils.js
+++
@@ -1,7 +0,0 @@
-export function normalizeText(text) {
-    return text
-      .trim()
-      .normalize("NFD") // separa caracteres com acento do caractere base
-      .replace(/[\u0300-\u036f]/g, "") // remove os sinais diacríticos
-      .toLowerCase();
-}
