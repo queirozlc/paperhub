f3eaa64b64ae8e81da04ba95c71340ab727a41d6
caslu <sepulchrolucas@gmail.com>
2025-05-23 18:57:11 -0300

    Implement AI tools dropdown and content type picker components
    This commit introduces two new components: `AiDropdown` and `ContentTypePicker`, enhancing the editor's functionality with AI tools and content type selection. Additionally, it adds a `Toggle` component for bold text formatting in the editor. Various dependencies have been updated for improved performance and compatibility.

---
+++ app/frontend/lib/components/ai-dropdown.svelte
@@ -0,0 +1,142 @@
+<script lang="ts" module>
+</script>
+
+<script lang="ts">
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu'
+  import Button from '$lib/components/ui/button/button.svelte'
+  import { ChevronDown } from '@voolt_technologies/untitledui-svelte'
+  import {
+    CircleSlash,
+    FileText,
+    Eraser,
+    Icon,
+    SparklesIcon,
+    Languages,
+    PencilLine,
+  } from '@lucide/svelte'
+
+  import type { Editor } from 'svelte-tiptap'
+
+  type Props = {
+    editor: Editor
+  }
+
+  type Item = {
+    slug: string
+    icon?: typeof Icon
+    label: string
+    onClick: () => void
+    subContent?: Item[]
+  }
+
+  let aiToolsDropdownContent = $state<HTMLDivElement>(null)
+
+  let { editor }: Props = $props()
+
+  const items: Item[] = [
+    {
+      slug: 'simplify',
+      icon: CircleSlash,
+      label: 'Simplificar',
+      onClick: () => {
+        console.log('Simplificar')
+      },
+    },
+    {
+      slug: 'spell-check',
+      icon: Eraser,
+      label: 'Correção ortográfica',
+      onClick: () => {
+        console.log('Correção ortográfica')
+      },
+    },
+    {
+      slug: 'summarize',
+      icon: FileText,
+      label: 'Resumir',
+      onClick: () => {
+        console.log('Resumir')
+      },
+    },
+    {
+      slug: 'translate',
+      icon: Languages,
+      label: 'Traduzir',
+      onClick: () => {
+        console.log('Traduzir')
+      },
+      subContent: [
+        {
+          slug: 'translate-to-english',
+          label: 'Inglês',
+          onClick: () => {
+            console.log('Para inglês')
+          },
+        },
+      ],
+    },
+    {
+      slug: 'complete-sentence',
+      icon: PencilLine,
+      label: 'Completar frase',
+      onClick: () => {
+        console.log('Completar frase')
+      },
+    },
+  ]
+</script>
+
+<DropdownMenu.Root
+  onOpenChange={(open) => {
+    if (open && aiToolsDropdownContent) {
+      aiToolsDropdownContent.focus()
+    }
+  }}
+>
+  <DropdownMenu.Trigger>
+    <Button
+      size="sm"
+      variant="ghost"
+      class="gap-2 items-center not-[svg]:text-primary/90 hover:text-primary"
+    >
+      <SparklesIcon class="size-4" />
+      <div class="flex items-center gap-1">
+        <span class="text-sm font-medium font-brand">AI Tools</span>
+        <ChevronDown class="size-3" />
+      </div>
+    </Button>
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content
+    onOpenAutoFocus={(e) => e.preventDefault()}
+    bind:ref={aiToolsDropdownContent}
+    sideOffset={4}
+  >
+    <DropdownMenu.Group class="text-muted-foreground">
+      {#each items as item (item.slug)}
+        {#if item.subContent}
+          <DropdownMenu.Sub>
+            <DropdownMenu.SubTrigger class="cursor-pointer">
+              <item.icon class="size-4" />
+              <span class="text-sm font-medium font-brand">{item.label}</span>
+            </DropdownMenu.SubTrigger>
+            <DropdownMenu.SubContent sideOffset={8}>
+              {#each item.subContent as subItem (subItem.slug)}
+                <DropdownMenu.Item onclick={subItem.onClick}>
+                  <subItem.icon class="size-4" />
+                  <span class="text-sm font-medium font-brand"
+                    >{subItem.label}</span
+                  >
+                </DropdownMenu.Item>
+              {/each}
+            </DropdownMenu.SubContent>
+          </DropdownMenu.Sub>
+        {:else}
+          <DropdownMenu.Item onclick={item.onClick}>
+            <item.icon class="size-4" />
+            <span class="text-sm font-medium font-brand">{item.label}</span>
+          </DropdownMenu.Item>
+        {/if}
+      {/each}
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
