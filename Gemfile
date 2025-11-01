787c69d2e3b6a946dc3e8e18d095e48e0bf97299
caslu <sepulchrolucas@gmail.com>
2025-11-01 02:20:43 -0300

    Integrate Yjs for collaborative document editing
    - Updated dependencies to include Yjs and related packages for real-time collaboration features.
    - Implemented SyncChannel for ActionCable to handle document synchronization.
    - Modified DocumentsController to support Yjs state management and syncing.
    - Enhanced Document show page to initialize Yjs document and syncer.
    - Updated Gemfile and Gemfile.lock to include new gems for Yjs integration.
    - Adjusted database schema to store document content as binary for Yjs compatibility.
    - Added styles for collaboration cursors in the editor.
    
    This commit lays the groundwork for collaborative editing capabilities in the application.

--- Gemfile
+++ Gemfile
@@ -76,6 +76,10 @@ gem "y-rb_actioncable"
 ### ========== Logs and Monitouring =================
 gem "appsignal"
 
+### ========== Editor Sync ==========
+gem "y-rb"
+gem "y-rb_actioncable"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/frontend/lib/components/documents/show/editor-header/tabs.svelte
+++ app/frontend/lib/components/documents/show/editor-header/tabs.svelte
@@ -27,7 +27,8 @@
       prefetch
       replace
       preserveState={false}
-      only={['documents']}
+      only={['document']}
+      replace
     >
       <TabsTrigger class="px-2" value="editor">
         <Icon class="size-5" name="file" />
@@ -39,7 +40,8 @@
       prefetch
       replace
       preserveState={false}
-      only={['documents', 'file_content']}
+      only={['document']}
+      replace
     >
       <TabsTrigger class="px-2" value="git">
         <Icon class="size-5" name="source-control" />
