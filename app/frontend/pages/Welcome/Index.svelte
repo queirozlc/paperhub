1d1de6bf64f6e562b69a3c368948d0bc3076eba6
caslu <91512054+queirozlc@users.noreply.github.com>
2025-11-06 09:20:55 -0300

    add a button to go back to home from editor and source control (#155)

--- app/frontend/pages/Welcome/Index.svelte
+++ app/frontend/pages/Welcome/Index.svelte
@@ -4,8 +4,17 @@
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen">
-  <h1 class="text-4xl font-bold">Welcome</h1>
+  <h1 class="text-4xl font-bold">Bem-vindo</h1>
+  <h3 class="text-accent-foregorund text-center mt-2">
+    🚧 Página em construção... 🚧
+  </h3>
+  <span class="text-muted-foreground text-center mt-2">
+    Ainda estamos implementando algumas funcionalidades no sistema
+  </span>
+  <span class="text-muted-foreground text-center">
+    mas fique à vontade para explorar o que já está disponível!
+  </span>
   <Link href="/users/sign_in" class="mt-4">
-    <Button>Sign in</Button>
+    <Button>Ir para Login</Button>
   </Link>
 </div>
